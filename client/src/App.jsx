671a9478f020d00fcc19c000209b5f752ee207e7
dannybanie <danielbrooks0000@gmail.com>
2024-01-20 15:06:13 +0100

    installed react-icons and react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import { Button } from "flowbite-react";
+import { GoHomeFill } from "react-icons/go";
 
 export default function App() {
   return (
     <div className="text-red-500">
       {/* flowbite test */}
       <Button> Enter </Button>
+      {/* React icons test */}
+      <GoHomeFill />
     </div>
   )
 }
