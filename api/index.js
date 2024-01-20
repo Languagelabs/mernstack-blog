97981b8d33143ebef98e743d3dd8cbd892eb12e0
dannybanie <danielbrooks0000@gmail.com>
2024-01-20 16:46:12 +0100

    created a test user endpoint and its working fine

--- api/index.js
+++ api/index.js
@@ -14,7 +14,13 @@ mongoose.connect(process.env.MONGO)
     console.log('Failed to connect to Mongodb' + err)
 })
 
+app.get('/api/user/test', (req, res) => {
+    res.json({message: 'User end point working '})
+})
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
+
+
 
