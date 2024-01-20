f59f5e569b40680cb98cf7e9dff49f3cc1fef57b
dannybanie <danielbrooks0000@gmail.com>
2024-01-20 15:17:24 +0100

    created components folder and pages folder in src and basic routing in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
-import { Button } from "flowbite-react";
-import { GoHomeFill } from "react-icons/go";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Posts from "./pages/Posts"
+import Videos from "./pages/Videos"
+import Podcasts from "./pages/Podcasts"
+import Channels from "./pages/Channels"
+import Search from "./pages/Search"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 
 export default function App() {
   return (
-    <div className="text-red-500">
-      {/* flowbite test */}
-      <Button> Enter </Button>
-      {/* React icons test */}
-      <GoHomeFill />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts/>} />
+        <Route path="/videos" element={<Videos/>} />
+        <Route path="/podcasts" element={<Podcasts/>} />
+        <Route path="/channels" element={<Channels/>} />
+        <Route path="/search" element={<Search/>} />
+        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/sign-up" element={<SignUp/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Podcasts.jsx
@@ -0,0 +1,6 @@
+
+export default function Podcasts() {
+  return (
+    <div>Podcasts</div>
+  )
+}
